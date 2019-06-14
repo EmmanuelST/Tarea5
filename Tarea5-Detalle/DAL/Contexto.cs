6dbd923def3867c171cf2c0d8511c3ff10e59116
Emmanuel Suarez Taveras <manuel.413@hotmail.com>
2019-06-14 16:06:50 -0400

    Agregando el stringConnection y la clase Contexto con los DbSet de Usuario y Analisis

---
+++ Tarea5-Detalle/DAL/Contexto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tarea5_Detalle.Entidades;
+
+namespace Tarea5_Detalle.DAL
+{
+    class Contexto : DbContext
+    {
+        public DbSet<Usuarios>Usuario { get; set; }
+        public DbSet<Analisis>Analisi { get; set; }
+        public Contexto() : base("Constr")
+        {
+
+        }
+    }
+}
