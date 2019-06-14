52e7e573afb2d45f1c7f34ca2928ea579f5b4656
Emmanuel Suarez Taveras <manuel.413@hotmail.com>
2019-06-14 17:15:58 -0400

    Trabajando en la interfaz de registro de usuario

---
+++ Tarea5-Detalle/UI/rUsuarios.Designer.cs
@@ -0,0 +1,261 @@
+﻿namespace Tarea5_Detalle.UI
+{
+    partial class rUsuarios
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(rUsuarios));
+            this.label1 = new System.Windows.Forms.Label();
+            this.IdnumericUpDown = new System.Windows.Forms.NumericUpDown();
+            this.label2 = new System.Windows.Forms.Label();
+            this.NombretextBox = new System.Windows.Forms.TextBox();
+            this.EmailtextBox = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.UsuariotextBox = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.ClavetextBox = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.FechaIngresodateTimePicker = new System.Windows.Forms.DateTimePicker();
+            this.Eliminarbutton = new System.Windows.Forms.Button();
+            this.Guardarbutton = new System.Windows.Forms.Button();
+            this.Nuevobutton = new System.Windows.Forms.Button();
+            this.Buscarbutton = new System.Windows.Forms.Button();
+            this.errorProvider = new System.Windows.Forms.ErrorProvider(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.IdnumericUpDown)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(25, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(19, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Id:";
+            // 
+            // IdnumericUpDown
+            // 
+            this.IdnumericUpDown.Location = new System.Drawing.Point(109, 29);
+            this.IdnumericUpDown.Maximum = new decimal(new int[] {
+            100000,
+            0,
+            0,
+            0});
+            this.IdnumericUpDown.Name = "IdnumericUpDown";
+            this.IdnumericUpDown.Size = new System.Drawing.Size(135, 20);
+            this.IdnumericUpDown.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(23, 63);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(47, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Nombre:";
+            // 
+            // NombretextBox
+            // 
+            this.NombretextBox.Location = new System.Drawing.Point(109, 63);
+            this.NombretextBox.Name = "NombretextBox";
+            this.NombretextBox.Size = new System.Drawing.Size(205, 20);
+            this.NombretextBox.TabIndex = 3;
+            // 
+            // EmailtextBox
+            // 
+            this.EmailtextBox.Location = new System.Drawing.Point(109, 100);
+            this.EmailtextBox.Name = "EmailtextBox";
+            this.EmailtextBox.Size = new System.Drawing.Size(205, 20);
+            this.EmailtextBox.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(23, 100);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Email:";
+            // 
+            // UsuariotextBox
+            // 
+            this.UsuariotextBox.Location = new System.Drawing.Point(109, 140);
+            this.UsuariotextBox.Name = "UsuariotextBox";
+            this.UsuariotextBox.Size = new System.Drawing.Size(205, 20);
+            this.UsuariotextBox.TabIndex = 8;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(23, 140);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(46, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Usuario:";
+            // 
+            // ClavetextBox
+            // 
+            this.ClavetextBox.Location = new System.Drawing.Point(111, 180);
+            this.ClavetextBox.Name = "ClavetextBox";
+            this.ClavetextBox.Size = new System.Drawing.Size(205, 20);
+            this.ClavetextBox.TabIndex = 10;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(25, 180);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(37, 13);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Clave:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(23, 231);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(93, 13);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "Fecha de Ingreso:";
+            // 
+            // FechaIngresodateTimePicker
+            // 
+            this.FechaIngresodateTimePicker.CustomFormat = "dd/MM/yyyy";
+            this.FechaIngresodateTimePicker.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.FechaIngresodateTimePicker.Location = new System.Drawing.Point(136, 225);
+            this.FechaIngresodateTimePicker.Name = "FechaIngresodateTimePicker";
+            this.FechaIngresodateTimePicker.Size = new System.Drawing.Size(178, 20);
+            this.FechaIngresodateTimePicker.TabIndex = 12;
+            // 
+            // Eliminarbutton
+            // 
+            this.Eliminarbutton.Image = global::Tarea5_Detalle.Properties.Resources.eliminar;
+            this.Eliminarbutton.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.Eliminarbutton.Location = new System.Drawing.Point(241, 278);
+            this.Eliminarbutton.Name = "Eliminarbutton";
+            this.Eliminarbutton.Size = new System.Drawing.Size(75, 52);
+            this.Eliminarbutton.TabIndex = 15;
+            this.Eliminarbutton.Text = "Eliminar";
+            this.Eliminarbutton.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.Eliminarbutton.UseVisualStyleBackColor = true;
+            // 
+            // Guardarbutton
+            // 
+            this.Guardarbutton.Image = global::Tarea5_Detalle.Properties.Resources.guardar;
+            this.Guardarbutton.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.Guardarbutton.Location = new System.Drawing.Point(138, 278);
+            this.Guardarbutton.Name = "Guardarbutton";
+            this.Guardarbutton.Size = new System.Drawing.Size(75, 52);
+            this.Guardarbutton.TabIndex = 14;
+            this.Guardarbutton.Text = "Guardar";
+            this.Guardarbutton.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.Guardarbutton.UseVisualStyleBackColor = true;
+            this.Guardarbutton.Click += new System.EventHandler(this.Guardarbutton_Click);
+            // 
+            // Nuevobutton
+            // 
+            this.Nuevobutton.Image = global::Tarea5_Detalle.Properties.Resources.nuevo;
+            this.Nuevobutton.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.Nuevobutton.Location = new System.Drawing.Point(30, 278);
+            this.Nuevobutton.Name = "Nuevobutton";
+            this.Nuevobutton.Size = new System.Drawing.Size(75, 52);
+            this.Nuevobutton.TabIndex = 13;
+            this.Nuevobutton.Text = "Nuevo";
+            this.Nuevobutton.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.Nuevobutton.UseVisualStyleBackColor = true;
+            this.Nuevobutton.Click += new System.EventHandler(this.Nuevobutton_Click);
+            // 
+            // Buscarbutton
+            // 
+            this.Buscarbutton.Image = global::Tarea5_Detalle.Properties.Resources.buscar;
+            this.Buscarbutton.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.Buscarbutton.Location = new System.Drawing.Point(270, 16);
+            this.Buscarbutton.Name = "Buscarbutton";
+            this.Buscarbutton.Size = new System.Drawing.Size(44, 41);
+            this.Buscarbutton.TabIndex = 4;
+            this.Buscarbutton.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.Buscarbutton.UseVisualStyleBackColor = true;
+            // 
+            // errorProvider
+            // 
+            this.errorProvider.ContainerControl = this;
+            this.errorProvider.Icon = ((System.Drawing.Icon)(resources.GetObject("errorProvider.Icon")));
+            // 
+            // rUsuarios
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(341, 352);
+            this.Controls.Add(this.Eliminarbutton);
+            this.Controls.Add(this.Guardarbutton);
+            this.Controls.Add(this.Nuevobutton);
+            this.Controls.Add(this.FechaIngresodateTimePicker);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.ClavetextBox);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.UsuariotextBox);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.EmailtextBox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.Buscarbutton);
+            this.Controls.Add(this.NombretextBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.IdnumericUpDown);
+            this.Controls.Add(this.label1);
+            this.Name = "rUsuarios";
+            this.Text = "rUsuarios";
+            ((System.ComponentModel.ISupportInitialize)(this.IdnumericUpDown)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox NombretextBox;
+        private System.Windows.Forms.Button Buscarbutton;
+        private System.Windows.Forms.TextBox EmailtextBox;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox UsuariotextBox;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox ClavetextBox;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.DateTimePicker FechaIngresodateTimePicker;
+        private System.Windows.Forms.Button Nuevobutton;
+        private System.Windows.Forms.Button Guardarbutton;
+        private System.Windows.Forms.Button Eliminarbutton;
+        private System.Windows.Forms.ErrorProvider errorProvider;
+        public System.Windows.Forms.NumericUpDown IdnumericUpDown;
+    }
+}
---
+++ Tarea5-Detalle/UI/rUsuarios.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Tarea5_Detalle.BLL;
+using Tarea5_Detalle.Entidades;
+
+namespace Tarea5_Detalle.UI
+{
+    public partial class rUsuarios : Form
+    {
+        public rUsuarios()
+        {
+            InitializeComponent();
+        }
+
+        private void Nuevobutton_Click(object sender, EventArgs e)
+        {
+            Limpiar();
+        }
+
+        private void Limpiar()
+        {
+            IdnumericUpDown.Value = 0;
+            NombretextBox.Text = string.Empty;
+            EmailtextBox.Text = string.Empty;
+            UsuariotextBox.Text = string.Empty;
+            ClavetextBox.Text = string.Empty;
+            FechaIngresodateTimePicker.Value = DateTime.Now;
+        }
+
+        private void Guardarbutton_Click(object sender, EventArgs e)
+        {
+
+            if (!Validar())
+                return;
+
+            Usuarios usuario = new Usuarios();
+            usuario = LlenarClase();
+
+            try
+            {
+                if (UsuariosBLL.Buscar((int)IdnumericUpDown.Value) != null)
+                {
+                    UsuariosBLL.Modificar(usuario);
+                    MessageBox.Show("Modificado correctamente");
+                }
+                else
+                {
+                    if (IdnumericUpDown.Value == 0)
+                    {
+                        UsuariosBLL.Guardar(usuario);
+                        MessageBox.Show("Guardado Correctamente");
+                    }
+                    else
+                    {
+                        MessageBox.Show("El Id debe ser 0 para guardar");
+                    }
+                }
+            }
+            catch(Exception)
+            {
+                MessageBox.Show("Hubo un error al intentar guardar");
+            }
+
+        }
+
+        private bool Validar()
+        {
+            errorProvider.Clear();
+            bool paso = true;
+
+            if(string.IsNullOrWhiteSpace(NombretextBox.Text))
+            {
+                errorProvider.SetError(NombretextBox, "Este campo no puede estar en blanco");
+                paso = false;
+            }
+
+            if (string.IsNullOrWhiteSpace(EmailtextBox.Text))
+            {
+                errorProvider.SetError(EmailtextBox, "Este campo no puede estar en blanco");
+                paso = false;
+            }
+
+            if (string.IsNullOrWhiteSpace(UsuariotextBox.Text))
+            {
+                errorProvider.SetError(UsuariotextBox, "Este campo no puede estar en blanco");
+                paso = false;
+            }
+
+            if (string.IsNullOrWhiteSpace(ClavetextBox.Text))
+            {
+                errorProvider.SetError(ClavetextBox, "Este campo no puede estar en blanco");
+                paso = false;
+            }
+
+            
+
+            return paso;
+        }
+
+        private Usuarios LlenarClase()
+        {
+            Usuarios usuarios = new Usuarios();
+
+            usuarios.UsuarioId = (int)IdnumericUpDown.Value;
+            usuarios.Nombre = NombretextBox.Text ;
+            usuarios.Email = EmailtextBox.Text;
+            usuarios.Usuario =  UsuariotextBox.Text;
+            usuarios.Clave =  ClavetextBox.Text;
+            usuarios.FechaIngreso =  FechaIngresodateTimePicker.Value;
+
+            return usuarios;
+        }
+    }
+}
