c8d47d3f3c1075fb28c95bf6acfe5ee560bdf54f
Emmanuel Suarez Taveras <manuel.413@hotmail.com>
2019-06-14 16:02:10 -0400

    Agregando la entidad TipoAnlisis

---
+++ Tarea5-Detalle/Entidades/TipoAnalisis.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tarea5_Detalle.Entidades
+{
+    class TipoAnalisis
+    {
+        [Key]
+        public int TipoAnalisisId { get; set; }
+        public string Descripcion { get; set; }
+
+        public TipoAnalisis()
+        {
+            TipoAnalisisId = 0;
+            Descripcion = string.Empty;
+        }
+    }
+}
