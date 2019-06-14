144c1b472743d3bd89f9ea05dcc033d705aee551
Emmanuel Suarez Taveras <manuel.413@hotmail.com>
2019-06-14 15:54:58 -0400

    Agregando la entidad Analisis

---
+++ Tarea5-Detalle/Entidades/Analisis.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tarea5_Detalle.Entidades
+{
+    class Analisis
+    {
+        public int AnalisisId { get; set; }
+        public DateTime Fecha { get; set; }
+        public int UsuarioId { get; set; }
+
+        public Analisis()
+        {
+            AnalisisId = 0;
+            Fecha = DateTime.Now;
+            UsuarioId = 0;
+        }
+    }
+}
