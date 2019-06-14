b672734988164f3905523ae8c15cb0963376966f
Emmanuel Suarez Taveras <manuel.413@hotmail.com>
2019-06-14 15:59:56 -0400

    Agregando la entidad AnalisisDetalles

---
+++ Tarea5-Detalle/Entidades/AnalisisDetalles.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tarea5_Detalle.Entidades
+{
+    class AnalisisDetalles
+    {
+        
+        public int AnalisisId { get; set; }
+        public int TipoId { get; set; }
+        public string Resultado { get; set; }
+
+        public AnalisisDetalles()
+        {
+            AnalisisId = 0;
+            TipoId = 0;
+            Resultado = string.Empty;
+        }
+    }
+}
